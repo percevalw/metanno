73721841772339003ab8af262d3db91cf3d7a3c4
Perceval Wajsburt <perceval.wajsburt@gmail.com>
2021-11-10 18:55:31 +0100

    v0.0.2

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ with open("README.md", "r") as fh:
 
 setup_args = dict(
     name="metanno",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/perceval/metanno",
     author="Perceval Wajsburt",
     author_email="perceval.wajsburt@sorbonne-universite.fr",
