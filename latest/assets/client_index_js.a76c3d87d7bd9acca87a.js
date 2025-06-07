d0ce72df6f7f06929407d45014df183d8e9159a4
Perceval Wajsbürt <perceval.wajsburt@aphp.fr>
2025-06-07 20:35:27 +0100

    Deployed c38efea to pret with MkDocs 1.6.1 and mike 2.1.3

---
+++ latest/assets/client_index_js.a76c3d87d7bd9acca87a.js
@@ -0,0 +1,20 @@
+"use strict";
+(self["webpackChunkpret_joy"] = self["webpackChunkpret_joy"] || []).push([["client_index_js"],{
+
+/***/ "./client/index.js":
+/*!*************************!*\
+  !*** ./client/index.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _globals__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./globals */ "./client/globals.ts");
+/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.css */ "./client/index.css");
+
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=client_index_js.a76c3d87d7bd9acca87a.js.map
