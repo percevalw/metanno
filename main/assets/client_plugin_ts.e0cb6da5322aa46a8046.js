8f53c6e1a848b92dcf959fbe853d9f8400684164
percevalw <percevalw@users.noreply.github.com>
2025-08-21 17:52:15 +0000

    Deployed 675082c to main with MkDocs 1.6.1 and mike 2.1.3

---
+++ main/assets/client_plugin_ts.e0cb6da5322aa46a8046.js
@@ -0,0 +1,30 @@
+"use strict";
+(self["webpackChunkmetanno"] = self["webpackChunkmetanno"] || []).push([["client_plugin_ts"],{
+
+/***/ "./client/plugin.ts":
+/*!**************************!*\
+  !*** ./client/plugin.ts ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _pret_globals__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @pret-globals */ "./client/dist-globals.ts");
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./style.css */ "./client/style.css");
+// noinspection ES6UnusedImports
+
+
+var plugin = {
+    id: "metanno:plugin", // app
+    activate: function () { return null; },
+    autoStart: true,
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=client_plugin_ts.e0cb6da5322aa46a8046.js.map
